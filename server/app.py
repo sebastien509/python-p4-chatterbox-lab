e803cdaea65433643a2c9eafdc9da4caacd74779
Sebastien <fenelonsebastien@outlook.com>
2024-12-13 12:50:28 -0500

    test compiled

--- server/app.py
+++ server/app.py
@@ -14,13 +14,97 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+def to_dict():
+    return{
+    }
+
+def find_message_by_id(id):
+    return Message.query.where(Message.id == id).first()
+
+
+@app.get('/messages')
 def messages():
-    return ''
+    all_messages = Message.query.all()
+
+    message_dicts=[ message.to_dict(['body','username', 'created_at']) for message in all_messages ]
+    
+    return message_dicts
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    found_message = Message.query.where(Message.id == id).first()
+
+    if found_message:
+        return found_message.to_dict(), 200
+    
+    else:
+         return {"status":404,"message": "Not found"}, 404
+
+@app.post('/messages')
+def posted_new_message():
+    Body = request.json 
+
+    try:
+
+        new_message = Message( body= Body.get('body'), username= Body.get('username'))
+
+        db.session.add( new_message)
+        db.session.commit()
+
+        return new_message.to_dict(), 201
+    
+    except Exception as error:
+
+        return {
+            "status": 400,
+            "message": "Something went really wrong... do you have an associated_llcs?",
+            "error_text": str(error)
+        }, 400
+    
+
+@app.patch('/messages/<int:id>')
+def update_message(id):
+    found_message= find_message_by_id(id)
+
+    if found_message:
+        try:
+            data = request.json
+
+            for key in data:
+                setattr(found_message, key, data[key])
+
+            db.session.add(found_message)
+            db.session.commit()
+
+            return found_message.to_dict(rules=['body', 'username']), 202
+        
+
+
+        except:
+                return {"status": 400, "message": "that didn't work"}, 400
+        
+    else: 
+        return {"status": 404, "message": "not found"}, 400
+
+
+@app.delete('/messages/<int:id>')
+def delete_message_by_id(id):
+    found_message = find_message_by_id(id)
+
+    if found_message:
+        db.session.delete(found_message)
+        db.session.commit()
+
+        return {"message": "Deleted successfully"}, 201
+    
+    else:
+        return {"status": 404 , "message": "Not found"}, 404
+
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.sql import func
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +14,8 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)   
+    username = db.Column(db.String, nullable=False )
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
